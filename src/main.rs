8159d9c5b41c5f77cab76e071b03e8a482fb0979
San <99511815+sanowl@users.noreply.github.com>
2024-05-24 15:22:19 +0300

    updated struc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+pub mod app;
+pub mod auth_handler;
+pub mod auth_routes;
+pub mod errros;
+pub mod invitation_handler;
+pub mod invitation_routes;
+pub mod models;
+pub mod register_routes;
+pub mod schema;
+pub mod utils;
+
 fn main() {
-    println!("Hello, world!");
+    print!("hell world =")
 }
