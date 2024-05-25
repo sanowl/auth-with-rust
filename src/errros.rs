5da39df024dd96f0e786ac9c4484dd4dcaab4027
San <99511815+sanowl@users.noreply.github.com>
2024-05-25 11:33:07 +0300

    updated dep  and updated code  errors with no  erros

--- src/errros.rs
+++ src/errros.rs
@@ -1,48 +1,51 @@
 use actix_web::{error::ResponseError, HttpResponse};
 use diesel::result::{DatabaseErrorKind, Error};
 use std::convert::From;
-use uuid::ParseError;
+use uuid::Error as UuidError;
 
-#[derive(Fail, Debug)]
+#[derive(Debug)]
 pub enum ServiceError {
-    #[fail(display = "Internal Server Error")]
     InternalServerError,
-
-    #[fail(display = "BadRequest: {})", _0)]
     BadRequest(String),
-
-    #[fail(display = "Unauthroized")]
     Unauthorized,
+    NotFound,
 }
 
 impl ResponseError for ServiceError {
     fn error_response(&self) -> HttpResponse {
-        match *self {
+        match self {
             ServiceError::InternalServerError => {
-                HttpResponse::InternalServerError().json("Internal Service Error")
+                HttpResponse::InternalServerError().json("Internal Server Error")
             }
             ServiceError::BadRequest(ref message) => HttpResponse::BadRequest().json(message),
             ServiceError::Unauthorized => HttpResponse::Unauthorized().json("Unauthorized"),
+            ServiceError::NotFound => HttpResponse::NotFound().json("Not Found"),
         }
     }
 }
 
-impl From<ParseError> for ServiceError {
-    fn from(_: ParseError) -> ServiceError {
+impl From<UuidError> for ServiceError {
+    fn from(_: UuidError) -> ServiceError {
         ServiceError::BadRequest("Invalid UUID".into())
     }
 }
-impl From<Error> for ServiceError{
-    fn from(erro:Error) ->ServiceError{
-        match error{
-            Error::DatabaseError(kind,info )=>{
-                if let DatabaseErrorKind::UniqueViolation = kind{
-                    let messafe= info.details().unwrap_or_else(|| info.message().to_string();
 
+impl From<Error> for ServiceError {
+    fn from(error: Error) -> ServiceError {
+        match error {
+            Error::DatabaseError(kind, info) => {
+                match kind {
+                    DatabaseErrorKind::UniqueViolation => {
+                        let message = info.details().unwrap_or_else(|| info.message()).to_string();
+                        ServiceError::BadRequest(message)
+                    }
+                    DatabaseErrorKind::NotFound => {
+                        ServiceError::NotFound
+                    }
+                    _ => ServiceError::InternalServerError,
                 }
-                ServiceError::InternalServerError
             }
-            _=>ServiceError::InternalServerError
+            _ => ServiceError::InternalServerError,
         }
     }
 }
