9376b2c6b149c6a872fe9a1771a2a998359f1f0b
San <99511815+sanowl@users.noreply.github.com>
2024-05-25 11:57:43 +0300

    updated rouets.rs

--- src/register_routes.rs
+++ src/register_routes.rs
@@ -1,25 +1,31 @@
-use actix_web::{AsyncResponder, FutureResponse, HttpResponse, Json, Path, ResponseError, State};
+use actix_web::{AsyncResponder, HttpResponse, Json, Path, ResponseError, State, web};
 use futures::future::Future;
-
 use crate::app::AppState;
 use crate::register_handler::{RegisterUser, UserData};
 
-pub fn register_user(
-    (invitaion_id, user_data, state): (Path<String>, Json<UserData>, State<AppState>),
-) -> FutureResponse<HttpResponse> {
+pub async fn register_user(
+    path: Path<String>,
+    user_data: Json<UserData>,
+    state: State<AppState>,
+) -> impl ResponseError<Error = actix_web::Error> {
+    // Extract the invitation_id from the path parameter
+    // Extract the user data (password) from the JSON payload
     let msg = RegisterUser {
-        // into_inner() returns the inner string value from Path
-        invitation_id: invitaion_id.into_inner(),
+        invitation_id: path.into_inner(),
         password: user_data.password.clone(),
     };
 
-    state
-        .db
-        .send(msg)
-        .from_err()
-        .and_then(|db_response| match db_response {
-            Ok(slim_user) => Ok(HttpResponse::Ok().json(slim_user)),
-            Err(err) => Ok(err.error_response()),
-        })
-        .responder()
+    // Send the RegisterUser message to the database actor
+    // and await the response
+    state.db.send(msg).await.map(|db_response| {
+        match db_response {
+            // If the response is successful, return an HTTP 200 OK response
+            // with the slim_user data as JSON
+            Ok(slim_user) => HttpResponse::Ok().json(slim_user),
+            
+            // If the response is an error, return an appropriate error response
+            // using the error_response() method
+            Err(err) => err.error_response(),
+        }
+    })
 }
